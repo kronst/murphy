84f9c4544f5f6ca89f738036420d10cbc17b17bd
Roman Konstantynovskyi <43999328+kronst@users.noreply.github.com>
2026-01-21 02:37:36 +0200

    ref: Clean up delay effects (#9)

--- murphy-core/src/main/kotlin/io/murphy/core/effect/DelayEffect.kt
+++ murphy-core/src/main/kotlin/io/murphy/core/effect/DelayEffect.kt
@@ -1,7 +1,17 @@
 package io.murphy.core.effect
 
 import io.murphy.core.Effect
+import io.murphy.core.MurphyContext
+import io.murphy.core.MurphyResponse
 
 sealed class DelayEffect : Effect {
     abstract val duration: Long
+
+    override fun apply(context: MurphyContext): MurphyResponse? {
+        val d = duration
+        if (d > 0) {
+            Thread.sleep(d)
+        }
+        return null
+    }
 }
--- murphy-core/src/main/kotlin/io/murphy/core/effect/LatencyEffect.kt
+++ murphy-core/src/main/kotlin/io/murphy/core/effect/LatencyEffect.kt
@@ -1,14 +1,3 @@
 package io.murphy.core.effect
 
-import io.murphy.core.MurphyContext
-import io.murphy.core.MurphyResponse
-
-internal class LatencyEffect(override val duration: Long) : DelayEffect() {
-
-    override fun apply(context: MurphyContext): MurphyResponse? {
-        if (duration > 0) {
-            Thread.sleep(duration)
-        }
-        return null
-    }
-}
+internal class LatencyEffect(override val duration: Long) : DelayEffect()

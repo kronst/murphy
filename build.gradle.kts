1bb2eda0668111c141cb6b1fffcaaf6f4ae2cefd
Roman Konstantynovskyi <43999328+kronst@users.noreply.github.com>
2026-01-23 03:15:09 +0200

    feat: Add release workflow (#11)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,41 @@
+import org.jreleaser.model.Active
+
+plugins {
+    id("org.jreleaser") version "1.22.0"
+}
+
 group = "io.github.kronst"
+
+jreleaser {
+    project {
+        copyright.set("Roman Konstantynovskyi")
+        name.set("murphy")
+        description.set("Chaos Engineering library for JVM")
+
+        links {
+            homepage.set("https://github.com/kronst/murphy")
+        }
+    }
+    signing {
+        active.set(Active.ALWAYS)
+        pgp {
+            armored.set(true)
+        }
+    }
+    deploy {
+        maven {
+            mavenCentral {
+                create("sonatype") {
+                    active.set(Active.ALWAYS)
+                    url.set("https://central.sonatype.com/api/v1/publisher/")
+                    namespace.set("io.github.kronst")
+                    stagingRepository("build/staging-deploy")
+                }
+            }
+        }
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(layout.buildDirectory)
+}
--- buildSrc/src/main/kotlin/murphy.kotlin-library.gradle.kts
+++ buildSrc/src/main/kotlin/murphy.kotlin-library.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
+
+group = rootProject.group
 
 val libs: VersionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
@@ -18,6 +21,51 @@ kotlin {
     jvmToolchain(17)
 }
 
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                name.set(project.name)
+                description.set("Chaos Engineering library for JVM - ${project.name}")
+                url.set("https://github.com/kronst/murphy")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("kronst")
+                        name.set("Roman Konstantynovskyi")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/kronst/murphy.git")
+                    developerConnection.set("scm:git:ssh://github.com/kronst/murphy.git")
+                    url.set("https://github.com/kronst/murphy")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri(rootProject.layout.buildDirectory.dir("staging-deploy"))
+        }
+    }
 }
